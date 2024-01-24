15c7b9eea6590c64192ab270d7b190eb840f1f07
abc-hi <revathimohancse@gmail.com>
2024-01-23 22:16:40 -0800

    changes done

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function changeImage(val)
+var imgages =display.querySelector("img")
+for(let ind in images)
+{
+    if(ind(val))
+    {
+        images[ind].src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fstock.adobe.com%2Fsearch%3Fk%3Dstar&psig=AOvVaw0PspON_M2XeT_5RgxG6_jD&ust=1705826219797000&source=images&cd=vfe&ved=0CBMQjRxqFwoTCODlrs7I64MDFQAAAAAdAAAAABAE" alt="Click me" onclick="alert(1)">" 
+     }
+}
